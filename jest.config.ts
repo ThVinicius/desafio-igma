4441cdd16f61e5e30f187fc45cf911e802a411f9
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-02-14 16:04:48 -0300

    build: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,38 +4,19 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  verbose: true,
   coveragePathIgnorePatterns: [
     'node_modules',
-    'test-config',
-    'interfaces',
-    'repositories',
-    'jestGlobalMocks.ts',
     '<rootDir>/src/server.ts',
     '<rootDir>/src/app.ts',
-    '<rootDir>/src/database',
+    '<rootDir>/src/databases',
     '<rootDir>/src/controllers',
     '<rootDir>/src/middlewares',
     '<rootDir>/src/repositories',
     '<rootDir>/src/routes',
-    '<rootDir>/src/routers',
     '<rootDir>/src/schemas',
-    '<rootDir>/src/services/testsService.ts',
-    '<rootDir>/src/utils',
-    '<rootDir>/tests',
-    '<rootDir>/prisma'
-  ],
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest'
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  verbose: true
+    '<rootDir>/src/utils'
+  ]
 }
 
 export default config
