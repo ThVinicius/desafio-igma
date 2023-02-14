a3b1173ad6a19056edb4bdb954d7add5b06ae524
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-02-14 12:48:08 -0300

    feat: CPF validation

---
+++ src/middlewares/cpfValidator.ts
@@ -0,0 +1,70 @@
+import { Request, Response, NextFunction } from 'express'
+
+export class CpfValidator {
+  use(req: Request, res: Response, next: NextFunction) {
+    const cpf: string = req.body.cpf
+
+    const cpfWithoutPunctuation = cpf.replaceAll('.', '').replace('-', '')
+
+    const cpfSlit = cpfWithoutPunctuation.split('').map(Number)
+
+    const secondDigitOfCpf = cpfSlit[10]
+    const firstTenCpfNumbers = cpfSlit.slice(0, 10)
+
+    const firstDigitOfCpf = cpfSlit[9]
+    const firstNineCpfNumbers = cpfSlit.slice(0, 9)
+
+    const firstDigitValidation: boolean = CpfValidator.digitValidation(
+      firstNineCpfNumbers,
+      firstDigitOfCpf
+    )
+    if (firstDigitValidation) return res.status(422).send('CPF inválido')
+
+    const secondDigitValidation: boolean = CpfValidator.digitValidation(
+      firstTenCpfNumbers,
+      secondDigitOfCpf
+    )
+
+    if (secondDigitValidation) return res.status(422).send('CPF inválido')
+
+    const cpfWithPunctuation = cpfWithoutPunctuation.replace(
+      /(\d{3})(\d{3})(\d{3})(\d{2})/,
+      '$1.$2.$3-$4'
+    )
+
+    req.body.cpf = cpfWithPunctuation
+
+    next()
+  }
+
+  private static digitValidation(cpf: number[], digit: number) {
+    const initialMultiplier = cpf.length + 1
+
+    const cPFNumbersMultipliedAndAdded = CpfValidator.multiplyAndAddTheArray(
+      cpf,
+      initialMultiplier
+    )
+
+    const restOf11 = cPFNumbersMultipliedAndAdded % 11
+
+    return this.splitValidation(restOf11, digit)
+  }
+
+  private static splitValidation(rest: number, digit: number) {
+    const firstValidation = rest < 2 && digit !== 0
+    const secondValidation = rest >= 2 && 11 - rest !== digit
+
+    return firstValidation || secondValidation
+  }
+
+  private static multiplyAndAddTheArray(
+    cpf: number[],
+    initialMultiplier: number
+  ) {
+    return cpf.reduce((acc, curr) => {
+      const aux = curr * initialMultiplier + acc
+      initialMultiplier--
+      return aux
+    }, 0)
+  }
+}
