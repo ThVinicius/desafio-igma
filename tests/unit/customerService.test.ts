8ad59f222c9753b8d2d632b2994d1b75f38c35bb
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-02-15 00:31:56 -0300

    refactor: customer factory

--- tests/unit/customerService.test.ts
+++ tests/unit/customerService.test.ts
@@ -7,13 +7,11 @@ describe('create method tests', () => {
     const repository = service.getRepository()
     const customer = new Customer()
 
-    jest
-      .spyOn(repository, 'create')
-      .mockResolvedValueOnce(customer.factoryWithId())
+    jest.spyOn(repository, 'create').mockResolvedValueOnce(customer.withId())
 
-    const response = await service.create(customer.factory())
+    const response = await service.create(customer.get())
 
-    expect(response).toEqual(customer.factoryWithId())
+    expect(response).toEqual(customer.withId())
     expect(repository.create).toBeCalled()
   })
 })
@@ -28,11 +26,11 @@ describe('findAllWithPagination method tests', () => {
 
     jest
       .spyOn(repository, 'findAllWithPagination')
-      .mockResolvedValueOnce([customer.factoryWithId()])
+      .mockResolvedValueOnce([customer.withId()])
 
     const response = await service.findAllWithPagination(skip, take)
 
-    expect(response).toEqual([customer.factoryWithId()])
+    expect(response).toEqual([customer.withId()])
     expect(repository.findAllWithPagination).toBeCalled()
   })
 })
@@ -43,13 +41,11 @@ describe('findByCpf method tests', () => {
     const repository = service.getRepository()
     const customer = new Customer()
 
-    jest
-      .spyOn(repository, 'findByCpf')
-      .mockResolvedValueOnce(customer.factoryWithId())
+    jest.spyOn(repository, 'findByCpf').mockResolvedValueOnce(customer.withId())
 
-    const response = await service.findByCpf(customer.factoryWithId().cpf)
+    const response = await service.findByCpf(customer.withId().cpf)
 
-    expect(response).toEqual(customer.factoryWithId())
+    expect(response).toEqual(customer.withId())
     expect(repository.findByCpf).toBeCalled()
   })
 
@@ -60,7 +56,7 @@ describe('findByCpf method tests', () => {
 
     jest.spyOn(repository, 'findByCpf').mockResolvedValueOnce(null)
 
-    const promise = service.findByCpf(customer.factory().cpf)
+    const promise = service.findByCpf(customer.get().cpf)
 
     expect(promise).rejects.toEqual({
       code: 'Not Found',
